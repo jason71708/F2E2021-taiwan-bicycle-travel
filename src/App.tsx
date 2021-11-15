5f5c84ed73e2f839dd98fc14dac30ad214baf1cc
Jason Zhuang <jason_zhuang@staging.com.tw>
2021-11-15 16:50:22 +0800

    feat: add leaflet plugin

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from 'react'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import { Town } from './types/api'
 import { fetchRequest } from './utils/api'
+
+const test: [number, number][] = [
+  [24.178695, 120.64501],
+  [24.167898, 120.638895],
+  [24.161438, 120.648701]
+]
 
 function App() {
   const [code, setCode] = useState<Town[]>([])
@@ -16,6 +23,25 @@ function App() {
   return (
     <div>
       <h1>App</h1>
+      <MapContainer
+        style={{ height: 600, width: 1200 }}
+        center={[24.172421, 120.6481]}
+        zoom={13}
+        scrollWheelZoom={true}
+        zoomControl={false}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {test.map((tes, index) => (
+          <Marker key={index} position={tes}>
+            <Popup>
+              My position <br /> {tes.join(', ')}
+            </Popup>
+          </Marker>
+        ))}
+      </MapContainer>
       <code>
         {JSON.stringify(code)}
       </code>
