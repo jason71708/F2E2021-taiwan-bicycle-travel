eba967bc058e11ef92f3a5aa2c904e8b45288ca0
Jason Zhuang <jason_zhuang@staging.com.tw>
2021-11-16 18:16:09 +0800

    feat: add icon cdn

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,11 +1,47 @@
 const theme = {
+  typography: {
+    h1: '45px',
+    h2: '36px',
+    h3: '32px',
+    h4: '36px',
+    h5: '22px',
+    content: '18px',
+    button: '15px',
+    caption: '12px'
+  },
   colors: {
-    primary: '#FF1D6C',
-    secondary: '#FFB72C',
-    tertiary: '#007350',
-    backgroud: '#F6F7FB',
-    dark: '#0D0B0C',
-    gray: '#ACACAC'
+    primary: {
+      100: '#DFE4CE',
+      200: '#CBD3B0',
+      300: '#A4B375',
+      400: '#738047',
+      500: '#474F2C',
+      600: '#292E19'
+    },
+    accent: {
+      100: '#FAF0E5',
+      200: '#F5DFC6',
+      300: '#EABD8A',
+      400: '#DE994A',
+      500: '#C67A24',
+      600: '#A3651E'
+    },
+    alert: {
+      100: '#F7D4DD',
+      200: '#F1B2C1',
+      300: '#F27594',
+      400: '#E75578',
+      500: '#BC3253',
+      600: '#8C3046'
+    },
+    grey: {
+      100: '#FFFFFF',
+      200: '#EEEEEE',
+      300: '#C5C5C5',
+      400: '#9A9A9A',
+      500: '#767676',
+      600: '#464646'
+    }
   }
 }
 
